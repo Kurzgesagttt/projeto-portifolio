7289f97d2b624c8f775dc9fe4dec74aba0c96745
LucasMoreno <Lucasmorenorodrigues@hotmail.com>
2025-02-06 20:39:50 -0300

    Containers adicionados e paginas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './containers/home';
+import About from './containers/about';
+import Contact from './containers/contact';
+import Resume from './containers/resume';
+import Portifolio from './containers/portifolio';
+import Skills from './containers/skills';
 
 function App() {
   return (
     <div className="App">
-      
+      {/* particles js*/}
+
+      {/* navbar */}
+
+      {/* main page content */}
+      <Routes>
+        <Route index path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/resume' element={<Resume />} />
+        <Route path='/portifolio' element={<Portifolio />} />
+        <Route path='/skills' element={<Skills />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+
+
     </div>
   );
 }
-
 export default App;
---
+++ src/containers/portifolio/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Portifolio=()=>{
+    return(
+        <div>
+            Portifolio page
+        </div>
+    )
+}
+export default Portifolio;
