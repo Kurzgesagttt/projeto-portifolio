e95e9323ba812f56e847d9341efe1b258db569e2
LucasMoreno <Lucasmorenorodrigues@hotmail.com>
2025-02-17 13:41:00 -0300

    About me TODO

--- src/containers/about/index.jsx
+++ src/containers/about/index.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { BsInfoCircleFill } from "react-icons/bs";
+import PageHeaderContent from "../../components/pageHeaderContent"
 
 const About=()=>{
     return(
-        <div>
-            About page
-        </div>
+        <section id="about" className="about">
+            <PageHeaderContent
+                headerText = "About me"
+                icon ={<BsInfoCircleFill size={40}/>}
+            />
+        </section>
     )
 }
 export default About;
